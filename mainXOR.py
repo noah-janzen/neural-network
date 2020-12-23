97ac72bc6fa352cd603d385968224092b0cefbfe
Noah Janzen <janzen@weiyu-digital.com>
2020-12-23 15:02:11 +0100

    Refactoring

--- mainXOR.py
+++ mainXOR.py
@@ -1,70 +1,67 @@
-from matrix import Matrix
 from neuralNetwork import NeuralNetwork
-# plot
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1)
-# number of input, hidden, output nodes
+# 1 Setup
+# ==============================
+
+# initialize neural network
 inputNodes = 2
 hiddenNodes = 16
 outputNodes = 1
 
-# learning rate
 learningRate = 0.3
 
-# create instance of a neural network
 neuralNetwork = NeuralNetwork(inputNodes, hiddenNodes, outputNodes, learningRate)
 
+# TRAINING DATA
 # load training data CSV file into a list
 training_data_file = open("data/XOR.csv", 'r')
 training_data_list = training_data_file.readlines()
 training_data_file.close()
 
-# visualize training data
+# parse training_data_list
 x1_training = []
 x2_training = []
-z_training = []
+y_training = []
 for record in training_data_list:
     x1_training.append(float(record.split(',')[0]))
     x2_training.append(float(record.split(',')[1]))
-    z_training.append(float(record.split(',')[2]))
+    y_training.append(float(record.split(',')[2]))
+
+# visualize training data
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-ax.scatter(x1_training, x2_training, z_training, marker='o',  c='r')
+ax.scatter(x1_training, x2_training, y_training, marker='o', c='r')
 ax.set_xlabel('x1')
 ax.set_ylabel('x2')
-ax.set_zlabel('z')
+ax.set_zlabel('y')
 plt.title("Trainingsdaten")
 plt.show()
 
-# 2)
-# train the neural network
+# 2 Train the neural network
+# ==============================
+
 epochs = 3000
 
-for e in range(epochs):
-    for i in range(len(training_data_list)):
-        # grab next record with index 0 to length of training_data_list - 1
-        record = training_data_list[i]
-
+for epoch in range(epochs):
+    for record in training_data_list:
         # split the record by the ',' semicolon and parse to float values
         values = record.split(',')
         # parse x1 and x2
-        input_value = [float(values[0]), float(values[1])]
+        input_vector = [float(values[0]), float(values[1])]
         # parse output
         target_value = [float(values[2])]
 
         # train neural network
-        neuralNetwork.train(input_value, target_value)
+        neuralNetwork.train(input_vector, target_value)
 
-        # print status
-        iteration = e * len(training_data_list) + i + 1
-        if (iteration % 1000) == 0:
-            print("iteration: ", iteration)
+    # print status
+    if ((epoch + 1) % 500) == 0:
+        print("epoch: ", epoch + 1)
 
-# 3) Visualize test data
-# test the neural network
+# 3 Test the neural network
+# ==============================
 
 # generate test values
 step_width = 0.01
@@ -89,6 +86,7 @@ for i in range(len(x1_test)):
     # append temp to y
     y_test.append(temp)
 
+# Visualize test result
 x1_test, x2_test = np.meshgrid(x1_test, x2_test)
 y_test = np.array(y_test)
 
@@ -100,5 +98,3 @@ ax.set_xlabel('x1')
 ax.set_ylabel('x2')
 ax.set_zlabel('z')
 plt.show()
-
-pass
